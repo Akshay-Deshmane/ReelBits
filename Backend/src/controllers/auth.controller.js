1aec32d03c78b0c7ab37d4e14e12423ebc003b7f
Akshay-Deshmane <akshaydeshmane1087@gmail.com>
2026-03-21 09:28:52 +0530

    login user api setup

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -52,10 +52,52 @@ async function userRegisterController(req, res) {
 
 
 
+async function loginUser(req, res) {
+    
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({
+        email
+    })
+
+    if(!user) {
+        return res.status(400).json({
+            message : "Invalid Email or Password"
+        })
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if(!isPasswordValid) {
+        return res.status(400).json({
+            message : "Invalid password"
+        })
+    }
+
+    const token = jwt.sign({
+        id : user._id
+    }, process.env.JWT_SECRET);
+
+    res.cookie("token", token);
+
+    return res.status(200).json({
+        message : "User Login Successfully",
+        user : {
+            _id : user._id,
+            fullName : user.fullName,
+            email : user.email
+        },
+        token
+    })
+
+
+}
 
 
 
 
 
 
-module.exports = {userRegisterController};
+
+
+module.exports = {userRegisterController, loginUser};
